d0a1357656b06ab49f4d312728d4926ffff3d9a1
Mauricio Cartagena <mc973220@gmail.com>
2021-05-07 12:50:29 -0400

    agregando flutter web

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/pages/home_page.dart';
+import 'package:flutter_application_1/src/pages/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'home': (BuildContext context) => HomePage()
+        });
+  }
+}
