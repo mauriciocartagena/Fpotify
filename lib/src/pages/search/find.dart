819080de13cf0239739bb054c0f041a0fd33e1ec
Mauricio Cartagena <mc973220@gmail.com>
2021-10-25 17:24:34 -0400

    Se realizo el Widge de Search

---
+++ lib/src/pages/search/find.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Find extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return ThemeData(
+      appBarTheme: AppBarTheme(
+        backwardsCompatibility: false, // 1
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        backgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
+      ),
+      scaffoldBackgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
+      hintColor: Colors.white,
+      textTheme: TextTheme(
+        subtitle1: TextStyle(
+          color: Colors.white,
+        ),
+        headline6: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  String selectedResult;
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(11, 14, 17, 1.0),
+      child: Center(
+        child: Text(
+          selectedResult,
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+  final List<String> listExample;
+  Find(this.listExample);
+
+  List<String> recentList = ['Text 4', 'text 3'];
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestionList = [];
+    query.isEmpty
+        ? suggestionList = recentList
+        : suggestionList
+            .addAll(listExample.where((element) => element.contains(query)));
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            suggestionList[index],
+          ),
+          onTap: () {
+            selectedResult = suggestionList[index];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
--- lib/src/pages/search/search.dart
+++ lib/src/pages/search/search.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/pages/search/find.dart';
 import 'package:flutter_application_1/src/preferences_user/preferences_user.dart';
 
 class SearchPage extends StatefulWidget {
+  final List<String> list = List.generate(10, (index) => "text $index");
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
@@ -14,24 +17,42 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     prefs.ultimaPagina = _SearchPageState.routeName;
     return Scaffold(
-      backgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Color.fromRGBO(11, 14, 17, 0.0),
-        brightness: Brightness.dark,
-        title: Text(
-          'Search',
-          style: TextStyle(
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
+        backgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: Find(widget.list));
+              },
+            ),
+          ],
+          automaticallyImplyLeading: false,
+          backgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
+          brightness: Brightness.dark,
+          title: Text(
+            'Search',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ),
-      body: Center(
-          child: Text(
-        'Search Page',
-        style: TextStyle(color: Colors.white),
-      )),
-    );
+        body: ListView.builder(
+          itemCount: widget.list.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(
+                widget.list[index],
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                  backgroundColor: Color.fromRGBO(11, 14, 17, 1.0),
+                ),
+              ),
+            );
+          },
+        ));
   }
 }
